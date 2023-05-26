664f5efcf5d1736bd5e11504fe43338c699abcea
acerohernan <contacto.acero.hernan@gmail.com>
2023-05-25 19:20:11 -0500

    server(build): Rersources folder added to server deplyment process

--- infra/lib/infra-stack.ts
+++ infra/lib/infra-stack.ts
@@ -1,6 +1,7 @@
 import * as cdk from "aws-cdk-lib";
 import * as ec2 from "aws-cdk-lib/aws-ec2";
 import * as iam from "aws-cdk-lib/aws-iam";
+import * as route53 from "aws-cdk-lib/aws-route53";
 import { Construct } from "constructs";
 import { readFileSync } from "fs";
 import path = require("path");
@@ -54,24 +55,26 @@ export class InfraStack extends cdk.Stack {
 
     const dockerConfig = ec2.InitFile.fromAsset(
       "/app/docker-compose.yml",
-      path.resolve(
-        __dirname,
-        "..",
-        "..",
-        "apps",
-        "server",
-        "docker-compose.prod.yml"
-      ),
+      path.resolve(__dirname, "..", "resources", "docker-compose.yml"),
       {}
     );
 
     const envFile = ec2.InitFile.fromAsset(
       "/app/docker.env",
-      path.resolve(__dirname, "..", "..", "apps", "server", ".env.docker")
+      path.resolve(__dirname, "..", "resources", "docker.env")
+    );
+
+    const nginxConfig = ec2.InitFile.fromAsset(
+      "/app/nginx.conf",
+      path.resolve(__dirname, "..", "resources", "nginx.conf")
     );
 
     /* Create init configuratin instance */
-    const init = ec2.CloudFormationInit.fromElements(dockerConfig, envFile);
+    const init = ec2.CloudFormationInit.fromElements(
+      dockerConfig,
+      envFile,
+      nginxConfig
+    );
 
     /* Create the instance */
     const ec2Instance = new ec2.Instance(this, "ec2-instance", {
@@ -85,12 +88,12 @@ export class InfraStack extends cdk.Stack {
       machineImage: new ec2.AmazonLinuxImage({
         generation: ec2.AmazonLinuxGeneration.AMAZON_LINUX_2,
       }),
-      keyName: "mitienda-key-pair",
+      keyName: "ubuntu-key-pair",
       init,
     });
 
     const userDataScript = readFileSync(
-      path.resolve(__dirname, "user-data.sh"),
+      path.resolve(__dirname, "..", "resources", "user-data.sh"),
       "utf8"
     );
 
@@ -102,6 +105,18 @@ export class InfraStack extends cdk.Stack {
     new ec2.CfnEIPAssociation(this, "ec2-ip-association", {
       eip: elasticIP.ref,
       instanceId: ec2Instance.instanceId,
+    });
+
+    /* Get the hosted zone for "station.lat" */
+    const hostedZone = route53.HostedZone.fromLookup(this, "ec2-hosted-zone", {
+      domainName: "station.lat",
+    });
+
+    /* Add an A" Record with the actual elastic ip address of the instance */
+    new route53.ARecord(this, "ARecord", {
+      zone: hostedZone,
+      target: route53.RecordTarget.fromIpAddresses(elasticIP.ref),
+      recordName: "api-mitienda.station.lat",
     });
   }
 }
